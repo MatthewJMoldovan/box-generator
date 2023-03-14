f6925f3004e7deee304a9212093b8b4bb7371264
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-03-14 17:16:35 -0600

    box-generator

---
+++ src/components/boxComponent.js
@@ -0,0 +1,48 @@
+import React, { userState, useState } from "react";
+import "./style.css";
+
+
+const allBoxes = [{color:'red'}, {color: 'blue'},{color: 'green'},{color: 'black'}];
+
+
+const Box = (props) => {
+  const [color, setColor] = useState("");
+  const [selcectedBoxes, setSelectedBoxes] = useState(allBoxes)
+
+  console.log(selcectedBoxes);
+  
+  const handleNewBoxSubmit = (e) => {
+    e.preventDefault();
+    const newBox = { color };
+    const updatedBoxes = [...selcectedBoxes, newBox];
+    setSelectedBoxes(updatedBoxes)
+    setColor("")
+  };
+
+
+  return (
+    <div className="App">
+        <h1>Add a new box!</h1>
+        <form onSubmit={(e) =>{
+            handleNewBoxSubmit(e);
+        }}>
+            <label>Color: </label>
+            <input
+            type="text"
+            value={color}
+            onChange={(e) =>{
+                setColor(e.target.value)
+            }}/>
+            <button>Add</button>
+        </form>
+        <div className="boxCard">
+            {selcectedBoxes.map((box,i) => {
+                return <section key={i} className="box" style={{backgroundColor: box.color}}>
+                </section>
+            })}
+        </div>
+    </div>
+  );
+};
+
+export default Box;
